9b36a65a9b4ebd28c094ec769c556fd6621b39b1
Ph4nt <128416298+ph4ntomnader@users.noreply.github.com>
2025-12-09 11:27:22 +0000

    Basic data pull running

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import List, Optional, Tuple
 import warnings
@@ -15,19 +15,29 @@ except ImportError as err:
     warnings.warn("NexradAwsInterface could not be imported. Please check NexradAWS installation.")
 
 class NEXRADLoader:
-    def __init__(self, cache_dir: Optional[Path] = '/data/cache/', data_dir: Optional[Path] = '/data/raw/'):
+    def __init__(self, cache_dir: Optional[Path] = 'data/cache/', data_dir: Optional[Path] = 'data/raw/'):
         if not NEXRAD_AVAILABLE:
             raise ImportError("NexradAwsInterface is not available. Please check NexradAWS installation.")
+        
         self.nexrad = NexradAwsInterface()
-        self.cache_dir = cache_dir
+        self.cache_dir = Path(cache_dir)
+        self.data_dir = Path(data_dir)
         self.cache_dir.mkdir(parents=True, exist_ok=True)
+        self.data_dir.mkdir(parents=True, exist_ok=True)
 
-    def download_scans(self, station: 'KTLX', start_time: datetime, end_time: datetime) -> List[Path]:     # set station from which to download data in station: str argument
-        scans = self.nexrad.get_scans_in_range(station, start_time, end_time)
+    def download_scans(self, start_time: datetime, end_time: datetime, station: str = 'KTLX') -> List[Path]:     # set station from which to download data in station: str argument
+        scans = self.nexrad.get_avail_scans_in_range(start_time, end_time, station)
+        scan_results = self.nexrad.download(scans, basepath=self.data_dir)
         downloaded_files = []
-        for scan in scans:
-            local_path = self.cache_dir / scan.object_key
-            if not local_path.exists():
-                self.nexrad.download_scan(scan, str(local_path))
-            downloaded_files.append(local_path)
+        for scan in scan_results.success:
+                downloaded_files.append(Path(scan.filepath))
+            
         return downloaded_files
+    
+loader = NEXRADLoader()                         # testing the loader with small sample data
+end_time = datetime.now(timezone.utc)
+start_time = end_time - timedelta(hours=1)
+downloaded_files = loader.download_scans(start_time=start_time, end_time=end_time,station='KTLX')
+print(f"Downloaded files: {downloaded_files}")
+for file in downloaded_files:
+    print(file)
